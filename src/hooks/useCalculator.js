71af767adc7ea356c3a417acc57aab3f18e5af4f
Lim Yu_Jie <94518915+lim-yujie@users.noreply.github.com>
2023-07-18 12:41:41 +0800

    updated/prepped calculator hook file for dynamic coding later

--- src/hooks/useCalculator.js
+++ src/hooks/useCalculator.js
@@ -1,27 +1,5 @@
 import { useState } from 'react';
 
-const useCalculator = (initialData) => {
-  const [data, setData] = useState(initialData);
-
-  const incrementQuantity = (index) => {
-    setData(prevData => {
-      const newData = [...prevData];
-      newData[index].quantity += 1;
-      return newData;
-    });
-  };
-
-  const decrementQuantity = (index) => {
-    setData(prevData => {
-      const newData = [...prevData];
-      if (newData[index].quantity > 0) {
-        newData[index].quantity -= 1;
-      }
-      return newData;
-    });
-  };
-
-  return { data, incrementQuantity, decrementQuantity };
-};
+// insert code that changes calculator menu based on stall here
 
 export default useCalculator;
