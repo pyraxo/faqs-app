341005586f84c09fcbe2e06c7a0f3f66ecf3058d
janiceyohana <janiceyohana@gmail.com>
2023-08-01 00:13:38 +0800

    edit homepage and minor style change, rename stalls

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,47 +3,62 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import ChickenRice from "assets/chicken-rice.png";
+import stallsData from "assets/stalls.json";
 import "./style.css";
+
+import ChickenRiceImage from "assets/chicken-rice.png"
+import IndianImage from "assets/indian.png"
+import TaiWaneseImage from "assets/taiwanese.png"
+import HealthySoupImage from "assets/healthy-soup.png"
+import MixedRiceImage from "assets/mixed-rice.png"
+import DrinksImage from "assets/drinks.png"
+import BanMianImage from "assets/banmian.png"
+
+const getImageByStall = (stallName) => {
+  switch (stallName) {
+    case "Chicken Rice":
+      return ChickenRiceImage;
+    case "Indian":
+      return IndianImage;
+    case "TaiWanese":
+      return TaiWaneseImage;
+    case "Healthy Soup":
+      return HealthySoupImage;
+    case "Mixed Rice":
+      return MixedRiceImage;
+    case "Drinks & Snacks":
+      return DrinksImage;
+    case "Ban Mian":
+      return BanMianImage;
+  }
+};
 
 const Home = () => {
   return (
-    <div className="container">
+    <div>
       <div className="green-block"></div>
-      <div className="cards-container">
-        <Card className="card">
-          <CardMedia
-            component="img"
-            height="80px"
-            image={ChickenRice}
-            alt="Chicken Rice"
-            paddingBottom="5px"
-          />
-          <CardContent sx={{ padding: 0, paddingBottom: 0 }}>
-            <Typography variant="h6" component="div" padding="5px" paddingBottom="0" sx={{ fontWeight: "bold", textAlign: "center" }}>
-              Card 1
-            </Typography>
-            <Typography variant="body2" color="text.secondary" padding="5px" sx={{ textAlign: "center" }}>
-              This is the first card
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card className="card">
-          <CardMedia
-            component="img"
-            height="80px"
-            image={ChickenRice}
-            alt="Chicken Rice"
-          />
-          <CardContent sx={{ padding: 0, paddingBottom: 0 }}>
-            <Typography variant="h6" component="div" padding="5px" paddingBottom="0" sx={{ fontWeight: "bold", textAlign: "center" }}>
-              Card 2
-            </Typography>
-            <Typography variant="body2" color="text.secondary" padding="5px" sx={{ textAlign: "center" }}>
-              This is the second card
-            </Typography>
-          </CardContent>
-        </Card>
+      <div className="white-block">
+        <div className="cards-container" style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '10px' }}>
+          {stallsData.map((stall, index) => (
+            <Card key={index} className="card">
+              <CardMedia
+                component="img"
+                height="80px"
+                image={getImageByStall(stall.name)}
+                alt={stall.name}
+                paddingBottom="5px"
+              />
+              <CardContent sx={{ padding: 0, paddingBottom: 0 }}>
+                <Typography variant="h6" component="div" padding="5px" paddingBottom="0" sx={{ fontWeight: "bold", textAlign: "center" }}>
+                  {stall.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" padding="5px" sx={{ textAlign: "center" }}>
+                  This is the first card
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
