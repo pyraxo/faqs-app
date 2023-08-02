ebae43a83256be45feac1ba26964fa2371190404
pyraxo <pyraxo@tfwno.gf>
2023-08-02 22:30:05 +0800

    Merge prev and some refactor

--- src/pages/StallInfo/index.jsx
+++ src/pages/StallInfo/index.jsx
@@ -1,34 +1,43 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import './style.css';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import "./style.css";
 import ChickenRice from "assets/chicken-rice.png";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
-const StallInfo() {
+export default function StallInfo() {
   return (
     <div>
       <div className="stall-info-header">
         {/* Back Icon */}
-        <ArrowBackIosIcon style={{ fontSize: 30, color: '#ffffff' }} />
+        <ArrowBackIosIcon style={{ fontSize: 30, color: "#ffffff" }} />
         <div style={{ flexGrow: 1 }}></div>
         <Typography
           style={{
-            textDecoration: 'underline',
-            color: '#ffffff',
+            textDecoration: "underline",
+            color: "#ffffff",
             fontSize: 20,
           }}
-          className='justify-center'
+          className="justify-center"
         >
           help?
         </Typography>
       </div>
       <div className="green-block"></div>
       <div className="stall-info-container">
-        <Card style={{ width: '370px', borderRadius: '10px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', height: '300px', marginBottom: '20px', zIndex: 1 }}>
+        <Card
+          style={{
+            width: "370px",
+            borderRadius: "10px",
+            boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+            height: "300px",
+            marginBottom: "20px",
+            zIndex: 1,
+          }}
+        >
           <CardActionArea>
             <CardMedia
               component="img"
@@ -36,8 +45,19 @@ const StallInfo() {
               image={ChickenRice}
               alt="Chicken Rice"
             />
-            <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Typography gutterBottom variant="h5" component="div" style={{ fontWeight: 'bold' }}>
+            <CardContent
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                style={{ fontWeight: "bold" }}
+              >
                 Chicken Rice
               </Typography>
               <Typography variant="body2" color="text.secondary">
@@ -46,19 +66,49 @@ const StallInfo() {
             </CardContent>
           </CardActionArea>
         </Card>
-        <Card style={{ width: '370px', borderRadius: '10px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', marginBottom: '20px' }}>
+        <Card
+          style={{
+            width: "370px",
+            borderRadius: "10px",
+            boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+            marginBottom: "20px",
+          }}
+        >
           <CardActionArea>
-            <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <CardContent
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
               <Typography variant="body2" color="text.secondary">
                 Hainanese chicken rice specialty.
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
-        <Card style={{ width: '370px', borderRadius: '10px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)' }}>
+        <Card
+          style={{
+            width: "370px",
+            borderRadius: "10px",
+            boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+          }}
+        >
           <CardActionArea>
-            <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Typography gutterBottom variant="h5" component="div" style={{ fontWeight: 'bold', textDecoration: 'underline' }}>
+            <CardContent
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                style={{ fontWeight: "bold", textDecoration: "underline" }}
+              >
                 Menu
               </Typography>
               {/* Add the menu content here */}
@@ -73,6 +123,4 @@ const StallInfo() {
       </div>
     </div>
   );
-};
-
-export default StallInfo;
+}
