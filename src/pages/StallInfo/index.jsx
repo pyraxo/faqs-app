79627e3ee30315c0517532a2bf14994016029080
Lim Yu_Jie <94518915+lim-yujie@users.noreply.github.com>
2023-08-03 21:17:32 +0800

    fixed StallInfo UI

--- src/pages/StallInfo/index.jsx
+++ src/pages/StallInfo/index.jsx
@@ -4,11 +4,12 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+// import { CardActionArea } from "@mui/material";
 import "./style.css";
 import ChickenRice from "assets/chicken-rice.png";
 import BottomNavBar from "components/ExperimentNavbar";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import data from "../../assets/stalls.json";
 
 export default function StallInfo() {
   const navigate = useNavigate();
@@ -49,31 +50,29 @@ export default function StallInfo() {
             zIndex: 1,
           }}
         >
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="200px"
-              image={ChickenRice}
-              alt="Chicken Rice"
-            />
-            <CardContent
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
+          <CardMedia
+            component="img"
+            height="200px"
+            image={ChickenRice}
+            alt="Chicken Rice"
+          />
+          <CardContent
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              style={{ fontWeight: "bold" }}
             >
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                style={{ fontWeight: "bold" }}
-              >
-                Chicken Rice
-              </Typography>
-              <Typography variant="body2" color="text.secondary"/>
-            </CardContent>
-          </CardActionArea>
+              Chicken Rice
+            </Typography>
+            <Typography variant="body2" color="text.secondary"/>
+          </CardContent>
         </Card>
         <Card
           style={{
@@ -83,19 +82,17 @@ export default function StallInfo() {
             marginBottom: "20px",
           }}
         >
-          <CardActionArea>
-            <CardContent
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <Typography variant="body2" color="text.secondary">
-                Hainanese chicken rice specialty.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
+          <CardContent
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="body2" color="text.secondary">
+              Hainanese chicken rice specialty.
+            </Typography>
+          </CardContent>
         </Card>
         <Card
           style={{
@@ -104,25 +101,49 @@ export default function StallInfo() {
             boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
           }}
         >
-          <CardActionArea>
-            <CardContent
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                style={{ fontWeight: "bold", textDecoration: "underline" }}
-              >
-                Menu
-              </Typography>
-              {/* Add the menu content here */}
-            </CardContent>
-          </CardActionArea>
+          <CardContent
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            {/* {data.map((stall) => {
+              // Check if the stall has a menu
+              if (stall.menu) {
+                return (
+                  <div key={stall.name}>
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+                      component="div"
+                      style={{
+                        fontWeight: "bold",
+                        textDecoration: "underline",
+                        textAlign: "center"
+                      }}
+                    >
+                      {stall.name} Menu
+                    </Typography>
+                    <ul>
+                      {stall.menu.map((item) => (
+                        <li key={item.item}>
+                          {item.item} - {item.price}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                );
+              } else {
+                // If the stall does not have a menu
+                return (
+                  <Typography variant="body2" color="text.secondary" key={stall.name}>
+                    No menu available for {stall.name}.
+                  </Typography>
+                );
+              }
+            })} */}
+          </CardContent>
         </Card>
         <BottomNavBar />
       </div>
