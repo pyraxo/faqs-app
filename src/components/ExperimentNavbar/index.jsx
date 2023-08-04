8895515dc0c9017130f2714dc4c45325a56d56a4
Lim Yu_Jie <94518915+lim-yujie@users.noreply.github.com>
2023-08-05 00:00:05 +0800

    added confirmation dialog

--- src/components/ExperimentNavbar/index.jsx
+++ src/components/ExperimentNavbar/index.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import "./style.css";
 
 const BottomNavBar = () => {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
 
   const handleButtonClick = () => {
-    // Navigate to the EndingPage when the button is clicked
-    navigate('/end');
+    setOpen(true);
+  };
+
+  const handleConfirmation = (proceed) => {
+    setOpen(false);
+    if (proceed) {
+      navigate('/end');
+    }
   };
 
   return (
@@ -15,6 +28,22 @@ const BottomNavBar = () => {
       <button className="end-button" onClick={handleButtonClick}>
         I'm done deciding!
       </button>
+      <Dialog open={open} onClose={() => handleConfirmation(false)}>
+        <DialogTitle>Confirmation</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you have finished deciding?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleConfirmation(true)} color="error" autoFocus>
+            Yes
+          </Button>
+          <Button onClick={() => handleConfirmation(false)} color="primary">
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
