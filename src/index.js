925b88828ad57bde32f372448eff0e1591595fdd
pyraxo <pyraxo@tfwno.gf>
2023-08-06 01:59:05 +0800

    Update service worker logic

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme } from "@mui/material/styles";
 
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 const theme = createTheme({
@@ -23,6 +24,11 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
